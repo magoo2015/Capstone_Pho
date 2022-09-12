4cff1c12dd2af86851dab11121ec1c15bb51053a
Sydney McGee <mcgee.sydney@gmail.com>
2022-09-12 11:46:38 -0500

    Added restaurnatpage + working on searchbar functionality

--- frontend/src/components/SearchBar/SearchBar.jsx
+++ frontend/src/components/SearchBar/SearchBar.jsx
@@ -5,20 +5,31 @@ const SearchBar = (props) => {
     const [location, setLocation] = useState('')
     const [category, setCategory] = useState('')
 
+    const handleLocation = (event) => {
+        console.log(setLocation)
+        setLocation(event.target.value);
+    };
+
+    const handleCatergory = (event) => {
+        console.log(setCategory)
+        setCategory(event.target.value);
+    };
+
     return ( 
         <div className='searchbar-container'>
             <form>
                 <label className='search-location'>Choose Location</label>
-                <select name='locations' id='locations'>
-                    <option value={location}>Dallas</option>
-                    <option value={location}>Richardson</option>
-                    <option value={location}>Fort Worth</option>
+                <select value={location} onChange={handleLocation}>
+                    <option value='Dallas'>Dallas</option>
+                    <option value='Richardson'>Richardson</option>
+                    <option value='Fort Worth'>Fort Worth</option>
                 </select>
                 <label className='search-category'>Choose Category</label>
-                <select>
-                    <option value={category}>Vegan</option>
-                    <option value={category}>Vegetarian</option>
+                <select value={category} onChange={handleCatergory}>
+                    <option value='Vegan'>Vegan</option>
+                    <option value='Vegetarian'>Vegetarian</option>
                 </select>
+                <button className='search-button'>Search</button>
             </form>
         </div>
      );
