20527ec071e53a037ee92c53add283a6ea4334b8
Sydney McGee <mcgee.sydney@gmail.com>
2022-09-13 11:53:40 -0500

    Working on restaurantpage, getting missing dependency errors

--- frontend/src/pages/RestaurantPage/RestaurantPage.js
+++ frontend/src/pages/RestaurantPage/RestaurantPage.js
@@ -1,11 +1,37 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import AuthContext from '../../context/AuthContext';
+import { KEY } from '../../localKey';
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const RestaurantPage = (props) => {
 
     const {businessid} = useParams();
-    const {businesses} = useContext(AuthContext)
+    const {business, setBusiness} = useState({})
+    console.log(business)
+
+    const config = {
+        headers: {
+          "Access-Control-Allow-Credentials": true,
+          "Access-Control-Allow-Origin": "*",
+          Authorization: `Bearer ${KEY}`,
+        },
+      };
+
+    useEffect (() => {
+        const getBusiness = async () => {
+            try {
+                let response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${businessid}`, config);
+                setBusiness(response.data)
+            } catch (error) {
+                console.log(error)
+                
+            }
+        }
+        getBusiness()
+    }, [businessid])
+
+    
 
     return (
         <div className='restaurant-container'>
