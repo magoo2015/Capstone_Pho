c6e0546a70c4920da42645c0f57512f19e2eff9f
Sydney McGee <mcgee.sydney@gmail.com>
2022-09-17 23:36:45 -0500

    Trying to get restaurantpage to display with api again...

--- frontend/src/pages/RestaurantPage/RestaurantPage.js
+++ frontend/src/pages/RestaurantPage/RestaurantPage.js
@@ -12,45 +12,30 @@ import { GOOGLE } from '../../localKey';
 const RestaurantPage = (props) => {
 
     const {businessid} = useParams();
-    const [business, setBusiness] = useState(DATA1)
+    const [business, setBusiness] = useState({}); //change to DATA1 to local data
     const [user, token] = useAuth();
-   // console.log(business)
-   
+    console.log(businessid)
     
-
-    
-
-    useEffect (() => { 
-
+    useEffect(() => {
         const config = {
             headers: {
-              "Access-Control-Allow-Credentials": true,
-              "Access-Control-Allow-Origin": "*",
-              Authorization: `Bearer ${KEY}`,
+                "Access-Control-Allow-Credentials": true,
+                "Access-Control-Allow-Origin": "*",
+                Authorization: `Bearer ${KEY}`,
             },
-          };
-
+        };
 
         const getBusiness = async () => {
             try {
                 let response = await axios.get(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/${businessid}`, config);
-                setBusiness(response.data)
+                setBusiness(response.data);
             } catch (error) {
                 console.log(error)
-                
             }
-        }
-        //getBusiness()
-        //postRestaurant()
-        getReviews()
-        //postReviews()
+        };
+        getBusiness()
     }, [businessid])
-
-    async function getReviews(){
-        const response = await axios.get(`http://127.0.0.1:8000/api/reviews/restaurant_reviews/${businessid}/`);
-        console.log(response.data)
-    }
-
+   
     async function postRestaurant(){
         let newRestaurant = {
             pho_restaurant_id: businessid,
@@ -86,13 +71,11 @@ const RestaurantPage = (props) => {
 
     }
 
-
-
     return (
         <div className='restaurant-container'>
             <div className='restaurant'>
                 <h1>{business.name}</h1>
-                <img src={business.image_url} />
+                <img src={business.image_url} alt="Restaurant"/>
                 <p>{business.location.display_address}</p>
                 <p>{business.coordinates.latitude}</p>
                 <p>{business.coordinates.longitude}</p>
@@ -100,9 +83,9 @@ const RestaurantPage = (props) => {
                     <iframe className='rest-map' title='map'
                     width="450"
                     height="250"
-                    frameborder="0" style={{border:0}}
-                    referrerpolicy="no-referrer-when-downgrade"
-                    src={`https://www.google.com/maps/embed/v1/view?key=${GOOGLE}&center=${business.coordinates.latitude},${business.coordinates.longitude}`}>
+                    frameBorder="0" style={{border:0}}
+                    referrerPolicy="no-referrer"
+                    src={`https://www.google.com/maps/embed/v1/place?key=${GOOGLE}&q=${business.coordinates.latitude},${business.coordinates.longitude}`}>
                     </iframe>
                 </div>
             </div>
@@ -111,7 +94,7 @@ const RestaurantPage = (props) => {
             </div>
         </div>
 
-      );
+    );
 }
  
 export default RestaurantPage;
